1ccbe92a2f1256574bebf5b399b118daac956d86
Carla <113267384+RuixDsgn@users.noreply.github.com>
2023-06-19 21:35:17 -0700

    complete lab with all tests passing

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,19 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = self._sort_word(word)
+    
+    def match(self, word_list):
+        matches = []
+        for word in word_list:
+            if self._is_anagram(word):
+                matches.append(word)
+            else:
+                print([])
+        return matches
+    
+    def _sort_word(self, word):
+        return ''.join(sorted(word.lower()))
+    
+    def _is_anagram(self, word):
+        return self.sorted_word == self._sort_word(word)
